28f0345e79f713eba87e175c3f3f40ad6e582a89
Evgeni Rovneiko <erovneiko@mail.ru>
2021-02-20 12:50:17 +0300

    Corrections

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -1,5 +1,54 @@
 require 'rails_helper'
 
 RSpec.describe AnswersController, type: :controller do
+  let(:question) { create(:question) }
+  let(:answer) { create(:answer, question: question) }
 
+  describe 'GET #show' do
+    it 'renders view' do
+      get :show, params: { id: answer }
+      expect(response).to render_template :show
+    end
+  end
+
+  describe 'GET #new' do
+    it 'renders view' do
+      get :new, params: { question_id: question }
+      expect(response).to render_template :new
+    end
+  end
+
+  describe 'POST #create' do
+    context 'valid attributes' do
+      it 'saves a new answer' do
+        expect do
+          post :create, params: {
+            question_id: question,
+            answer: attributes_for(:answer)
+          }
+        end.to change(question.answers, :count).by(1)
+      end
+
+      it 'redirects to show' do
+        post :create, params: { question_id: question, answer: attributes_for(:answer) }
+        expect(response).to redirect_to assigns(:answer)
+      end
+    end
+
+    context 'invalid attributes' do
+      it 'does not save' do
+        expect do
+          post :create, params: {
+            question_id: question,
+            answer: attributes_for(:answer, :invalid)
+          }
+        end.to_not change(question.answers, :count)
+      end
+
+      it 're-renders new' do
+        post :create, params: { question_id: question, answer: attributes_for(:answer, :invalid) }
+        expect(response).to render_template :new
+      end
+    end
+  end
 end
