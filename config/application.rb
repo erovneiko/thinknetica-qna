a836ec9c9ef03664dfdf86b46ddd527ae655338a
Evgeni Rovneiko <erovneiko@mail.ru>
2022-02-18 16:01:39 +0300

    Lesson 20 - Cache

--- config/application.rb
+++ config/application.rb
@@ -44,5 +44,7 @@ module Qna
     end
 
     config.active_job.queue_adapter = :sidekiq
+
+    config.cache_store = :redis_store, 'redis://localhost:6379/0/cache', { expires_in: 90.minutes }
   end
 end
